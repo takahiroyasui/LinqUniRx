cb58c2d185f0db88cf4c7eedcb7b8666e0987088
takahiroyasui <takahiro@uniba.jp>
2019-06-07 14:22:42 +0900

    Timer追加

---
+++ Assets/UniRx/Timer/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Timer START");
+        Observable.Timer(TimeSpan.FromSeconds(3))
+            .Subscribe(_ => Debug.Log("OK"));
+    }
+}
